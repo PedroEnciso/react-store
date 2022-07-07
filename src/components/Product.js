aa5e7f52019ec10b3ddc42de77ab7e1a178d2f28
Pedro Enciso <penciso@ucsd.edu>
2022-07-07 07:16:45 -0700

    tidying up product styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,7 +15,7 @@ const Product = ({ product }) => {
       </ProductTop>
       <ProductLabel>
         <Title>{product.title}</Title>
-        <Price className="bg-light">{product.price}</Price>
+        <Price>${product.price}</Price>
         <Category className="text-light">{product.category}</Category>
       </ProductLabel>
       <ProductButton>add to cart</ProductButton>
@@ -27,13 +27,21 @@ const ProductContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+  padding: 1.25rem 1.5rem;
+  border-radius: 10px;
+  transition: box-shadow 0.4s;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
 `;
 
 const ProductTop = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 10px;
-  padding: 0.75rem 1rem 1.25rem;
+  padding: 0rem 1rem 1.25rem;
   overflow: hidden;
 `;
 
@@ -44,18 +52,21 @@ const IconContainer = styled.div`
 `;
 
 const ImageContainer = styled.div`
-  height: 20rem;
+  height: 17rem;
   padding: 1rem 0;
 `;
 
 const ProductLabel = styled.div`
   display: grid;
-  grid-template-columns: 2fr, 1fr;
-  grid-gap: 1rem;
+  column-gap: 1rem;
+  grid-template-areas:
+    "title price"
+    "category";
+  grid-template-columns: 4fr 1fr;
 `;
 
 const Title = styled.p`
-  grid-column: 1/2;
+  grid-area: "title";
   /* copied from google to keep the title from running in 2 lines */
   width: 100%; /* the element needs a fixed width (in px, em, %, etc) */
   overflow: hidden; /* make sure it hides the content that overflows */
@@ -65,12 +76,15 @@ const Title = styled.p`
 
 const Price = styled.p`
   width: fit-content;
-  padding: 0.25rem 0.25rem;
-  grid-column: 2/3;
+  justify-self: end;
+  grid-area: "price";
+  font-size: 1rem;
+  font-weight: 600;
 `;
 
 const Category = styled.p`
   font-size: 1rem;
+  grid-area: "category";
 `;
 
 const ProductButton = styled.button`
